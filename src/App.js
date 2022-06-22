2f53c1dbeaedeeb98d3695121fe48fb76384e1e9
jheepark <jhee.park.work@gmail.com>
2022-06-22 22:58:43 +1000

    super interactive amazing site wow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./mia_vlad.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mia and Vlad Wedding RSVP
         </a>
       </header>
     </div>
